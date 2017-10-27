42968a9f8a7510391d5588aec5147cae2b832e5e
bllli <me@lidengming.com>
2017-10-27 17:59:28 +0800

    + 前端vue 未完成

---
+++ frontend/src/components/PortalSet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ui styled accordion">
+    <div class="title"><i class="dropdown icon"></i>Portal List</div>
+    <div class="content">
+      <div class="accordion">
+        <!-- 这里循环标签 -->
+        <div class="title"><i class="dropdown icon"></i> 城市 </div>
+        <div class="content">
+          <div class="accordion">
+          <!-- 这里插入pos -->
+            <div class="title"><i class="dropdown icon"></i> po名 </div>
+            <div class="content">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PortalSet',
+    data () {
+      return {
+
+      }
+    },
+    mounted: function () {
+      const self = this
+      this.axios.get('/api/portals')
+    }
+  }
+</script>
